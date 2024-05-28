01adf0a28a7d3eb05ec4fe11b922d2fb533081e5
Scott Parkhill <scott.parkhill@gmail.com>
2024-05-28 15:23:30 -0400

    Minor formatting change and whitespace removal in pyfoma/__init__.py

--- src/pyfoma/__init__.py
+++ src/pyfoma/__init__.py
@@ -1,10 +1,12 @@
 from pyfoma.fst import *
 from pyfoma.fst import re as regex
 from pyfoma.paradigm import Paradigm
-from pyfoma.algorithms import concatenate, union, intersection, kleene_star, kleene_plus, difference, cross_product, compose, optional, \
-                                ignore, projected, inverted, reversed, reversed_e, minimized, minimized_as_dfa, determinized_as_dfa, determinized_unweighted, \
-                                _algorithms_to_add
-
+from pyfoma.algorithms import (
+    concatenate, union, intersection, kleene_star, kleene_plus, difference,
+    cross_product, compose, optional, ignore, projected, inverted, reversed,
+    reversed_e, minimized, minimized_as_dfa, determinized_as_dfa,
+    determinized_unweighted, _algorithms_to_add
+)
 
 __author__     = "Mans Hulden"
 __copyright__  = "Copyright 2022"
@@ -14,7 +16,6 @@ __version__    = "2.0"
 __maintainer__ = "Mans Hulden"
 __email__      = "mans.hulden@gmail.com"
 __status__     = "Prototype"
-
 
 # This project is (partially) documented using the Sphinx docstring convention
 
@@ -58,5 +59,3 @@ for mutating_name, original_func in _algorithms_to_add.items():
 
     # Finally, add the new method to the FST class
     setattr(FST, mutating_name, new_func)
-
-
